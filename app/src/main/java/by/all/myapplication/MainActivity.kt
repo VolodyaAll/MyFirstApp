33c6a2f2978d87486031da08d977a54c793256df
VolodyaAll <volodya22all@gmail.com>
2020-11-23 16:24:45 +0300

    Add landscape mod for activity_movie_details.xml & transmission between Main screen & Details screen

--- app/src/main/java/by/all/myapplication/MainActivity.kt
+++ app/src/main/java/by/all/myapplication/MainActivity.kt
@@ -1,11 +1,23 @@
 package by.all.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView1: TextView = findViewById(R.id.main_activity_text_view_1)
+        textView1.setOnClickListener { moveToDetailsScreen() }
+    }
+
+    private fun moveToDetailsScreen() {
+        val intent = Intent(this, MovieDetailsActivity::class.java)
+
+        startActivity( intent )
     }
 }
